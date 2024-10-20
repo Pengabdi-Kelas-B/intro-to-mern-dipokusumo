0c3d483fb4ac41cc0e4d32a447aeb621982b18a9
Dipo Kusumo <kusumo.dipo.dk@gmail.com>
2024-10-20 16:36:25 +0700

    completing backend and frontend of pokedex

--- pokedex/backend/generateJsonDB.js
+++ pokedex/backend/generateJsonDB.js
@@ -2,22 +2,48 @@ const fs = require("fs");
 const axios = require('axios').default
 
 async function generateJsonDB() {
-  // TODO: fetch data pokemon api dan buatlah JSON data sesuai dengan requirement.
-  // json file bernama db.json. pastikan ketika kalian menjalankan npm run start
-  // dan ketika akses url http://localhost:3000/pokemon akan muncul seluruh data
-  // pokemon yang telah kalian parsing dari public api pokemon
   const pokemonApiURL = "https://pokeapi.co/api/v2/pokemon/?limit=100";
 
-  const response = await axios.get(pokemonApiURL)
-  console.log(response.data.results);
+  try {
+    const response = await axios.get(pokemonApiURL);
+    const pokemonList = response.data.results;
 
-  const database = {
-    "pokemon": []
+    const database = { pokemon: [] };
+
+    for (const pokemon of pokemonList) {
+      const detail = await fetchPokemonDetail(pokemon.url);
+      database.pokemon.push(detail);
+    }
+
+    fs.writeFileSync("db.json", JSON.stringify(database, null, 4));
+    console.log("Database berhasil dibuat!");
+  } catch (error) {
+    console.error("Gagal generate JSON DB:", error);
   }
+}
 
-  database.pokemon = response.data.results
+async function fetchPokemonDetail(url) {
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
 
-  fs.writeFileSync('db.json', JSON.stringify(database, null, 4))
+    return {
+      id: data.id,
+      name: data.name,
+      types: data.types.map((type) => type.type.name),
+      abilities: data.abilities.map((ability) => ability.ability.name),
+      height: data.height,
+      weight: data.weight,
+      cries: {
+        latest: `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${data.id}.ogg`,
+        legacy: `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/legacy/${data.id}.ogg`,
+      },
+      evolutionChains: [data.name], // Dummy value karena data evolusi perlu fetch lagi dari API evolusi
+    };
+  } catch (error) {
+    console.error(`Gagal fetch detail untuk ${url}:`, error);
+    return null;
+  }
 }
 
 generateJsonDB(); 
